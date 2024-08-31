e2b4ff458ca65a42c9008a82582e44c29f329e1a
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2024-08-31 06:30:19 +0200

    Root route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,13 @@ import routes from './routes';
 const app = express();
 
 app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('API is running!');
+});
+
 app.use('/api', routes);
+
 export default app;
 
